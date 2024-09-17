c5e2b7e5ae78b8c0069cdf2993911a296c39c1e9
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2024-09-16 21:58:10 -0700

    update layou bar

---
+++ React-Native-Crash-Course/app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { View, Text, Image } from 'react-native'
+import { Tabs, Redirect } from 'expo-router';
+
+import {icons} from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused}) => {
+  return(
+    <View className='items-center justify-center gap-2'>
+      <Image 
+      source = {icon}
+      resizeMode= 'contain'
+      tintColor = {color}
+      className='w-6 h-6'
+      />
+      <Text  className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }} >
+      </Text>
+    </View>
+  )
+}
+
+const TabLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen name = 'home'
+        options = {{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({color, focused}) => (
+              <TabIcon
+              icon = { icons.home}
+              color = { color }
+              name = "Home"
+              focused={focused}
+              />
+        )
+        }} />
+      </Tabs>
+    </>
+  )
+}
+
+export default  TabLayout;
--- React-Native-Crash-Course/tailwind.config.js
+++ React-Native-Crash-Course/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaty: '#161622',
+        secondary: {
+          DEFAULT: "#FF9C01",
+          100: "FF9001",
+          200: "FF8E01",
+        },
+        black: {
+          DEFAULT:"",
+
+        },
+      }
+    },
   },
   plugins: [],
 }
