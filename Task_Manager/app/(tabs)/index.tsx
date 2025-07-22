f7fabeead8649e5cf09d20182a43615d16fd28b7
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2025-07-22 04:49:26 -0700

    Task_Manager_app
    Basic app to see a Task Manager shared with Collaborators.

---
+++ Task_Manager/app/(tabs)/index.tsx
@@ -0,0 +1,202 @@
+import { Feather } from '@expo/vector-icons';
+import React from 'react';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
+const data = [
+  {
+    label: '📅 Create project timeline',
+    date: new Date('2024-12-12'),
+    completed: true,
+  },
+  {
+    label: '🎯 Define project goals',
+    date: new Date('2025-01-05'),
+    completed: false,
+  },
+  {
+    label: '👥 Identify project stakeholders',
+    date: new Date('2025-01-09'),
+    completed: true,
+  },
+  {
+    label: '📝 Gather project requirements',
+    date: new Date('2025-01-12'),
+    completed: false,
+  },
+  {
+    label: '💰 Create project budget',
+    date: new Date('2025-01-15'),
+    completed: false,
+  },
+];
+
+const collaborators = [
+  {
+    name: 'David Z. (you)',
+    avatar:
+      'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+  },
+  {
+    name: 'Sierra K.',
+    avatar:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+  },
+  {
+    name: 'Jack W.',
+    avatar:
+      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+  },
+];
+
+const index = () => {
+  return (
+    <View style={{flex : 1 }}>
+      <SafeAreaView >
+        <View style = {styles.header}>
+          <View style = {styles.headerAction}>
+            <TouchableOpacity onPress={() => {
+                //handel onPress
+            }}>
+              <Feather name="chevron-left" color="#000" size={40}/> 
+            </TouchableOpacity>
+          </View>
+
+            <Text numberOfLines = {1} style = {styles.headerTitle}>My Task</Text>
+
+          <View style = {[styles.headerAction, {alignItems: 'flex-end'}]}>
+            <TouchableOpacity onPress={() => {
+                //handel onPress
+            }}>
+              <Feather name="more-vertical" color="#000" size={40}/> 
+            </TouchableOpacity>
+          </View>
+          </View>
+
+            <ScrollView style = {{paddingBottom: 50}}>
+              <View style = {styles.section}>
+                <Text style = {styles.title}>Project Planing</Text>
+
+                <View style = {styles.row}>
+                  <Text style = {styles.rowValue}>Due date</Text>
+                  <Text style = {styles.rowValue}>Jul 22, 2025</Text>
+                </View>
+
+                <View style = {styles.row}>
+                  <Text style = {styles.rowField}>Status</Text>
+                  <View style = {styles.badge}>
+                    <Text style = {styles.badgeText}>In Progress</Text>
+                  </View>
+                </View>
+                
+                <View style = {styles.row, {alignItems: 'flex-start'}}>
+                  <Text style = {styles.rowField}>Collaborators</Text>
+                  <View>
+                    {collaborators.map(({name, avatar}, index) => {
+                      return (
+                        <TouchableOpacity style = {styles.collaborator} onPress = {() => {
+                          //Handel Press
+                        }}>
+                          <Image alt = "" source = {{uri:avatar}} style = {styles.avatar}></Image>
+                          <Text style = {styles.collaboratorText}>{name}</Text>
+                        </TouchableOpacity>
+                      )
+                    })}
+                    
+                  </View>
+                </View>
+
+              </View>
+            </ScrollView>
+      </SafeAreaView>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create ({
+  title:{
+    fontSize: 23,
+    fontWeight: '600',
+    color: "#lelele",
+    paddingBottom: 10
+  },
+  header: {
+    flexDirection: 'row',
+    marginTop :70,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  headerAction: {
+    width: 40,
+    height: 40,
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  headerTitle: {
+    fontSize: 19,
+    fontWeight: '500',
+    color: "black",
+    flex: 1,
+    textAlign: 'center'
+  },
+  section: {
+    paddingHorizontal: 16,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#d7d7d7',
+    backgroundColor: '#ffff',
+    paddingTop: 12,
+    marginBottom: 8
+  },
+  row: {
+    flexDirection: 'row',
+    marginVertical: 8,
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  rowField: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#0e0e0e',
+    width: 130
+  },
+  rowValue: {
+    fontSize: 15,
+    marginRight: 25,
+    fontWeight: '500',
+    color: '#171717'
+  },
+  badge: {
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: "center",
+    backgroundColor: "#ffa500"
+  },
+  badgeText: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: "#fff"
+  },
+  avatar: {
+    width: 20,
+    height: 20,
+    borderRadius: 99,
+  },
+  collaborator: {
+    flexDirection: "row",
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  collaboratorText: {
+    fontSize: 13,
+    fontWeight: '500',
+    color: '#171717',
+    marginLeft: 6
+  },
+
+})
